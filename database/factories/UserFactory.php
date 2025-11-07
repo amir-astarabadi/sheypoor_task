304a9c01fc4293407f7ebd1bf382c64e4ed01ae2
Amir Pourastarabadi <amirpourastarabadi@gmail.com>
2025-11-07 17:18:18 +0330

    Update UserFactory to generate scores between 0 and 1000; modify DatabaseSeeder to create 50 users and update leaderboard scores.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,7 +27,7 @@ class UserFactory extends Factory
             'name' => fake()->unique()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'score' => random_int(0, 100),
+            'score' => random_int(0, 1000),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Services\LeaderBoard\LeaderBoardService;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,11 +16,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $leaderBoardService = resolve(LeaderBoardService::class);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::factory(50)
+            ->create()
+            ->each(function ($user) use ($leaderBoardService) {
+                $leaderBoardService->update($user->getLeaderBoardKey(), $user->getRawOriginal('score'));
+            });
     }
 }
